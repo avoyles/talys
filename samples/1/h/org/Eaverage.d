0bf336699cf9405ca5842f40bb528ce073b5ebfe
Andrew Voyles <andrew.voyles@gmail.com>
2023-12-06 13:23:28 -0800

    updating samples/ to v1.96

--- samples/1/h/org/Eaverage.d
+++
@@ -1,6 +0,0 @@
-# n +  93Nb Average deuteron emission energy
-# Q-value    =-3.81821E+00
-# 
-# # energies =     1
-#    E     E-average
- 1.40000E+01 7.85321E+00
--- samples/4/b/org/Eaverage.d
+++
@@ -1,6 +0,0 @@
-# n +  28Si Average deuteron emission energy
-# Q-value    =-9.43897E+00
-# 
-# # energies =     1
-#    E     E-average
- 2.00000E+01 6.35156E+00
